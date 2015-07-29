2478446afdac955377b1f2fef8c89b3317b08df7
waleed samy <waleed_samy@ymail.com>
2015-07-29 17:29:42 +0300

    nginx will host widget pages , now there are only one web service used to update widget content

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  platformTemplatesPath : '/home/sand/Kode/nKode/ubicallwidget/static/generated/',
+  platformTemplatesPath : '/var/www/widget/',
   serverPort : 7575,
-  //TODO REDIRECT IN platform.js
-  widgetError : 'https://cdn.ubicall.com/widget/error.html',
-  mainTemplate : './static/generated/template.html'
+  mainTemplate : './views/template.html'
 }
