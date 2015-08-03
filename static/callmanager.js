a58f580bf2ef8171d6c0b4f4ebbf70ea86453079
waleed samy <waleed_samy@ymail.com>
2015-08-03 21:15:41 +0300

    fix miss typeing

--- static/callmanager.js
+++ static/callmanager.js
@@ -91,7 +91,7 @@ var ubiCallManager = ubiCallManager || (function() {
       },
       success: function(response) {
         if (response.status == 200) {
-          _saveSipInfo(status.data);
+          _saveSipInfo(response.data);
         } else {
           console.log("error un able to get your sip credentials ");
         }
