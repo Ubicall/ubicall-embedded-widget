97399a95d341aa3a699d07a8b0813d8765b70fd4
ahmed yousef <ayousef@sandcti.com>
2015-11-19 15:23:41 +0200

    edit form screen type form

--- genWidget/htmlUtil.js
+++ genWidget/htmlUtil.js
@@ -248,7 +248,7 @@ if(form.__next){
 path =form.__next.id;
 }else{ path = home }
     var $form = $("<form/>").attr("id", "form-" + formId).attr("method", "post").attr("action", "")
-            .attr("onsubmit", "helpers.submitForm('form-" + formId + "','"+ path +"');return false;");
+            .attr("onsubmit", "helpers.submitForm('form-" + formId + "','"+ path +"','"+ form.ScreenType +"');return false;");
    
 
     form.FormFields.forEach(function(field) {
